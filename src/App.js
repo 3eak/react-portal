e40541a9b70fb23b090f5dadd0a55999cd7b040e
Stefan Cooper <stefan.cooper27@gmail.com>
2020-08-01 21:03:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+
+constructor(props) {
+  super(props);
+
+  this.state = { x: 0, y: 0 };
+}
+
+_onMouseMove(e) {
+  this.setState({ x: e.screenX, y: e.screenY });
+}
+
+// render() {
+//   const { x, y } = this.state;
+//   return <div onMouseMove={this._onMouseMove.bind(this)}>
+//     <h1>Mouse coordinates: { x } { y }</h1>
+//   </div>;
+// }
+
+render() {
+  const { x, y } = this.state;
+  return <div onMouseMove={this._onMouseMove.bind(this)}  className="App">
+      <h1>Mouse coordinates: { x } { y }</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +40,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
 }
+}
+
 
 export default App;
